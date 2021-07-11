a0f6c38b1ebc9df8aa2449ecf14ed60fb66a1cac
davies-jack <jackethdavies@hotmail.com>
2021-07-11 15:21:37 +0100

    refreshing access token endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,4 +27,22 @@ exports.login = (req, res, next) => {
         ).catch(err => res.json({ err }));
 };
 
-exports.refresh = (req, res, next) => { };
+exports.refresh = (req, res, next) => {
+    const { refreshToken } = req.body;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    });
+
+    spotifyApi
+        .refreshAccessToken()
+        .then(({ body: { access_token: accessToken, expires_in: expiresIn } }) => {
+            res.json({
+                accessToken,
+                expiresIn
+            })
+        })
+        .catch(err => res.json({ err }))
+};
