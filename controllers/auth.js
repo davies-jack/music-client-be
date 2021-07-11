967fc4655b8dcc80d3e0ab986d4338c59b502fad
davies-jack <jackethdavies@hotmail.com>
2021-07-11 15:17:42 +0100

    requesting auth code from spotifyApi and passing it back

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,30 @@
-const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require("spotify-web-api-node");
 
 exports.login = (req, res, next) => {
     const { code } = req.body;
     const spotifyApi = new SpotifyWebApi({
         redirectUri: process.env.REDIRECT_URI,
         clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET
+        clientSecret: process.env.CLIENT_SECRET,
     });
 
-    res.json({
-        hi: 'hi'
-    })
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(
+            ({
+                body: {
+                    access_token: accessToken,
+                    refresh_token: refreshToken,
+                    expires_in: expiresIn,
+                },
+            }) => {
+                res.json({
+                    accessToken,
+                    refreshToken,
+                    expiresIn
+                })
+            }
+        ).catch(err => res.json({ err }));
 };
 
-exports.refresh = (req, res, next) => {
-
-};
+exports.refresh = (req, res, next) => { };
