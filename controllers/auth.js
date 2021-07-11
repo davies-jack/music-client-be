d87a28d26001168003e7f352e5168dd827bd1d55
davies-jack <jackethdavies@hotmail.com>
2021-07-11 15:14:52 +0100

    bringing in spotify api within auth login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
-exports.login = (req, res, next) => {
+const SpotifyWebApi = require('spotify-web-api-node');
 
+exports.login = (req, res, next) => {
+    const { code } = req.body;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    });
+
+    res.json({
+        hi: 'hi'
+    })
 };
 
 exports.refresh = (req, res, next) => {
