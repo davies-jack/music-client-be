804a5badba6572f43459f8f293cbe98d7fdb5896
davies-jack <jackethdavies@hotmail.com>
2021-07-11 15:13:32 +0100

    accessing auth route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const app = require('express');
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
+const app = express();
+
+const authRouter = require('./routes/auth')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use('/auth', authRouter);
 
 
 const PORT = 8080;
